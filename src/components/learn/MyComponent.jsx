0f455565bc02a346b548fad163ed04f3872d560d
Mira Pham <thaochilam123@gmail.com>
2025-08-01 16:34:52 +0700

    video31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,11 +1,17 @@
 import './style.css';
 
 const MyComponent = () => {
+    const hoidanit = [1,2,3] //string
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age : 25
+    // }
     return (
         <>
-        <div>
-        eric & hoidanit
+        <div> 
+        {JSON.stringify(hoidanit)} & hoidanit
         </div>
+            <div>{ console.log ("ERIC")}</div>
         <div className='child' style={{borderRadius: "10px"}}>child</div>
       </>
   );
