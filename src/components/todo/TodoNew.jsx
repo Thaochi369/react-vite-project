b9d1edde139c51da9b1d445957ec905afae14325
Mira Pham <thaochilam123@gmail.com>
2025-08-02 21:14:38 +0700

    video36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,12 +3,27 @@ import './todo.css'
 const TodoNew = (props) => {
 
     const { addNewTodo } = props;
-    addNewTodo("name");
+    // addNewTodo("name");
+
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleChange = (event) => {
+        console.log (">>> handleOnchange ",event.target.value)
+    }
+
     return (
         <>
         <div className='todo-new'>
-            <input type="text" placeholder='Enter your task!'/>
-            <button>Add</button>
+                <input type="text"
+                    placeholder='Enter your task!'
+                    onChange={handleChange}
+                
+                />
+                <button style={{ cursor: 'pointer' }}
+                    onClick={handleClick}
+                >Add</button>
         </div>   
         </>
     )
