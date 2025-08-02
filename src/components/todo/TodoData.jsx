88394860b3dc474031711f82f6e99d0478540f3f
Mira Pham <thaochilam123@gmail.com>
2025-08-02 17:40:04 +0700

    video 34

---
+++ src/components/todo/TodoData.jsx
@@ -0,0 +1,17 @@
+import './todo.css'
+
+
+//
+const TodoData = (props) => {
+    return (
+        <>
+            <div className='todo-data'>
+                <div>My name is</div>
+                <div>Learning React</div>
+                <div>Watching Youtube</div>
+            </div>
+        </>
+    )
+}
+
+export default TodoData;
