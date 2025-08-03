d54799126b923f8f03e5f893c8dd1281bdcaf7e3
Mira Pham <thaochilam123@gmail.com>
2025-08-03 15:02:58 +0700

    video41

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,18 @@ const App = () => {
       <TodoNew
         addNewTodo = {addNewTodo}
       />
-      <TodoData
-        todoList = {todoList}
-      />
-      <div className='todo-image'>
-        <img className= "logo" src={reactLogo}/>
-      </div>
+
+
+      {todoList.length === 0 ?
+        
+        <div className='todo-image'>
+          <img className="logo" src={reactLogo} />
+        </div>
+        :
+        <TodoData
+          todoList={todoList}
+        />
+      }
     </div>
   )
 }
